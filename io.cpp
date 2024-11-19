033ca414237baf5942f3e9e24e221a241e7065dc
настя! <104497908+pekanool@users.noreply.github.com>
2024-11-19 14:53:59 +0300

    io.cpp

--- io.cpp
+++ io.cpp
@@ -2,11 +2,7 @@
 
 #include <iostream>
 
-void aba::print_array(const char* const comment, int* arr, const int size) {
-	const char format = ' ';
-	std::cout << comment;
-	for (int i = 0; i < size; i++) {
-		std::cout << format << arr[i];
-	}
-	std::cout << std::endl;
+void aba::print_array(arr) {
+	for (int num: arr) {
+		cout <<num<<" ";
 }
