1a844ab2a080c5c05f820544f6fb690cbc8a549b
настя! <104497908+pekanool@users.noreply.github.com>
2024-11-19 14:47:32 +0300

    main.cpp

--- main.cpp
+++ main.cpp
@@ -2,12 +2,12 @@
 #include "shell_sortings.hpp"
 
 int main() {
-    int arr[] = {1, 8, 2, 5, 3, 11};
-	const int arr_size = sizeof(arr) / sizeof(arr[0]);
+    vector <int> arr = {1, 8, 2, 5, 3, 11};
+	cout << "Исходный массив:";
 	
 	aba::print_array("Массив до сортировки:", arr, arr_size);
     
-    aba::sortings::shell_sort(arr, arr_size);
+    aba::sortings::shell_sort(arr);
 
     aba::print_array("Массив после сортировки:", arr, arr_size);
 
